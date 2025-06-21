68c244b53e4759b3960bcda3f9571e34c0b694df
Praveen B <you@example.com>
2025-06-21 12:48:17 +0530

    page navigation issues

--- next.config.mjs
+++ next.config.mjs
@@ -9,11 +9,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // GitHub Pages configuration
   basePath: process.env.NODE_ENV === 'production' ? '/Peer2PeerGameathon-5q' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Peer2PeerGameathon-5q/' : '',
   output: 'export',
-  trailingSlash: false,
-}
+  trailingSlash: true, // <-- IMPORTANT
+};
 
-export default nextConfig
+export default nextConfig;
